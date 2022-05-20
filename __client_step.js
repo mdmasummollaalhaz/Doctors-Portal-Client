9f73eec2fc98b7ac0ec6c3419fc014d68b16d774
Masum Mollo <freelancermasummolla@gmail.com>
2022-05-20 15:28:46 +0600

    init

---
+++ __client_step.js
@@ -0,0 +1,10 @@
+/**
+ * --------------------------
+ * Client site setup
+ * --------------------------
+ * 1. npx create-react-app
+ * 2. npm install react-router-dom@6 ( https://reactrouter.com/docs/en/v6/getting-started/installation#basic-installation )
+ * 3. npm install -D tailwindcss (https://tailwindcss.com/docs/installation)
+ * 4. npx tailwindcss init
+ * 5. 
+ */
