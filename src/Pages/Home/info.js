bc2428909b8fb1e756e8de5c9b0395ced8cbdb73
Masum Mollo <freelancermasummolla@gmail.com>
2022-05-22 21:54:21 +0600

    services

--- src/Pages/Home/info.js
+++ src/Pages/Home/info.js
@@ -7,9 +7,9 @@ import phone from '../../assets/icons/phone.svg'
 const info = () => {
 
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Opening Hours" cardDes="Lorem Ipsum is simply dummy text of the pri" img={clock}></InfoCard>
-            <InfoCard bgClass="bg-accent" cardTitle="Visit our location" cardDes="Brooklyn, NY 10036, United States" img={marker}></InfoCard>
+            <InfoCard bgClass="bg-primary" cardTitle="Visit our location" cardDes="Brooklyn, NY 10036, United States" img={marker}></InfoCard>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Contact us now" cardDes="+000 123 456789" img={phone}></InfoCard>
         </div>
     );
