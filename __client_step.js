f2c835fb4ce4f577f9f25ce79af803276563a739
Masum Mollo <freelancermasummolla@gmail.com>
2022-05-22 15:03:48 +0600

    Components and banner

--- __client_step.js
+++ __client_step.js
@@ -6,5 +6,6 @@
  * 2. npm install react-router-dom@6 ( https://reactrouter.com/docs/en/v6/getting-started/installation#basic-installation )
  * 3. npm install -D tailwindcss (https://tailwindcss.com/docs/installation)
  * 4. npx tailwindcss init
- * 5. 
+ * 5. npm i daisyui (https://daisyui.com/docs/install/)
+ * 6. Pages create & Route
  */
